843ab9667e31334438c4570b5fb69c70e4f468d4
zikrya <zikrya8@gmail.com>
2025-01-31 19:30:53 -0500

    chore: install tailwindcss dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,11 @@ function App() {
 
   return (
     <>
-\
+    <p class="text-sm ...">The quick brown fox ...</p>
+<p class="text-base ...">The quick brown fox ...</p>
+<p class="text-lg ...">The quick brown fox ...</p>
+<p class="text-xl ...">The quick brown fox ...</p>
+<p class="text-2xl ...">The quick brown fox ...</p>
     </>
   )
 }
